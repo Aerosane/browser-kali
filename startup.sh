445e13404ad7253396016f953650d54d0174014f
AerosaneDev <shouhong645@gmail.com>
2024-03-12 15:28:28 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,4 +6,4 @@ sleep 5
 #openbox-session&
 xfce4-session&
 x11vnc -display :1 -nopw -listen localhost -xkb -forever &
-cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900
+cd /root/noVNC && ln -s vnc.html index.html && ./utils/launch.sh --vnc localhost:5900
